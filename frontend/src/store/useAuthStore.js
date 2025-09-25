f0fc64ddcc01fe527fa61ded49cd4692e181d0d7
Sachin-kumar18 <162020286+Sachin-kumar18@users.noreply.github.com>
2025-09-25 17:57:29 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,8 +3,8 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const SOCKET_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chatapp-backend-m7bh.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -93,10 +93,9 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL, {
-      query: {
-        userId: authUser._id,
-      },
+    const socket = io(SOCKET_URL, {
+      withCredentials: true,
+      query: { userId: authUser._id },
     });
     socket.connect();
 
